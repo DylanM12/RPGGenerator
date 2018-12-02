d10349b164cf3f120745f736458d28829c395e90
Dylan McConnell <dmcconn7@uncc.edu>
2018-12-02 02:50:41 +0000

    started on delete for campaigns

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,15 +1,15 @@
 class CampaignsController < ApplicationController
     def create
-        @campaign = Campaign.find(params[:campaign_id])
-        @campaign = @campaign.campaigns.create(campaign_params)
-        redirect_to campaign_path(@campaign)
+        @character = Character.find(params[:character_id])
+        @campaign = @character.campaigns.create(campaign_params)
+        redirect_to character_path(@character)
     end
     
     def destroy
-        @campaign = Campaign.find(params[:id])
+        @character = Character.find(params[:character_id])
+        @campain = @character.campaigns.find(params[:id])
         @campaign.destroy
-        
-        redirect_to campaigns_path
+        redirect_to character_path(@character)
     end
     
     private
