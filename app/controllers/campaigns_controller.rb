af9478ad47bae197f1e647f7d72492d20808a796
Dylan McConnell <dmcconn7@uncc.edu>
2018-12-02 02:10:04 +0000

    fixed issues with campaigns saving

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,12 +1,19 @@
 class CampaignsController < ApplicationController
     def create
-        @character = Character.find(params[:character_id])
-        @campaign = @character.campaigns.create(campaign_params)
-        redirect_to character_path(@character)
+        @campaign = Campaign.find(params[:campaign_id])
+        @campaign = @campaign.campaigns.create(campaign_params)
+        redirect_to campaign_path(@campaign)
+    end
+    
+    def destroy
+        @campaign = Campaign.find(params[:id])
+        @campaign.destroy
+        
+        redirect_to campaigns_path
     end
     
     private
         def campaign_params
-            params.require(:campaign).permit(:title, :body)
+            params.require(:campaign).permit(:title, :info)
         end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_022028) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string "title"
-    t.text "info"
+    t.text "text"
     t.integer "character_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
